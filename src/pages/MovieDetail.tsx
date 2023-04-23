f7e52a11dbb5ce7bcff55278fac20fa7b52a170f
jasonlee6789 <jeffersonlee@163.com>
2023-04-23 09:28:12 +0900

    feat: - Adding favorites icon (react-icons and  Saving favorites and Removing favorites using local storage.

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card, Typography } from 'antd'
 import { Movie } from '../interfaces/MovieInterface'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 interface MovieProps {
 	data: Movie
 }
 
 const MovieDetail: React.FC<MovieProps> = ({ data }) => {
 	const { Title, Text } = Typography
+	const [favorites, setFavorites] = useState<Movie[]>([])
+	const isFavorite = favorites.some((fav) => fav.imdbID === data.imdbID)
+
+	useEffect(() => {
+		const storedFavorites = localStorage.getItem('favorites')
+		if (storedFavorites) {
+			setFavorites(JSON.parse(storedFavorites))
+		}
+	}, [])
+
+	const handleLikeClick = (movie: Movie) => {
+		if (!favorites.some((fav) => fav.imdbID === movie.imdbID)) {
+			setFavorites([...favorites, movie])
+			localStorage.setItem(
+				'favorites',
+				JSON.stringify([...favorites, movie])
+			)
+		} else {
+			const filteredFavorites = favorites.filter(
+				(fav) => fav.imdbID !== movie.imdbID
+			)
+			setFavorites(filteredFavorites)
+			localStorage.setItem('favorites', JSON.stringify(filteredFavorites))
+		}
+	}
 	return (
 		<Card style={{ width: '100%' }}>
 			<a
@@ -34,6 +60,18 @@ const MovieDetail: React.FC<MovieProps> = ({ data }) => {
 						Year: {data.Year}
 					</Text>
 				</div>
+
+				{isFavorite ? (
+					<AiFillHeart
+						onClick={() => handleLikeClick(data)}
+						style={{ fontSize: 30 }}
+					/>
+				) : (
+					<AiOutlineHeart
+						onClick={() => handleLikeClick(data)}
+						style={{ fontSize: 30 }}
+					/>
+				)}
 			</a>
 		</Card>
 	)
